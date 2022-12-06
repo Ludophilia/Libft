d868bb3eb1abf3e66b3cf3164fdc68ec0f798fe4
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2022-12-06 23:38:49 +0100

    Completed ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 14:59:24 by jgermany          #+#    #+#             */
-/*   Updated: 2022/12/06 17:28:23 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/12/06 23:29:31 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 // MEMORY LEAK SAFE OR NOT ?
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
 	size_t	lenset;
-	size_t	lens1;
+	size_t	i;
 	char	*str;
-	char	*start;
-	char	*end;
-	// Maybe would be simpley with trad i and j ??
 
+	start = 0;
+	end = ft_strlen(s1);
 	lenset = ft_strlen(set);
-	lens1 = ft_strlen(s1);
-	start = (char *)s1;
-	end = (char *)s1 + lens1;
-	if (ft_strnstr(start, set, lenset))
-		start = (char *)s1 + lenset;
-	if (ft_strnstr((end - lenset), set, lenset))
-		end = (char *)s1 + lens1 - lenset;
-	str = malloc(((uintptr_t)end - (uintptr_t)start + 1) * sizeof(char));
+	if (ft_strnstr(s1, set, lenset))
+		start = lenset;
+	if (ft_strnstr(s1 + end - lenset, set, lenset))
+		end -= lenset;
+	str = malloc((end - start + 1) * sizeof(char));
 	if (!str)
 		return ((char *)0);
-	while (str)
-		// ???
-	// Newstr with set at start and/or at end
+	i = 0;
+	while (i < (end - start))
+		str[i++] = s1[start + i];
+	str[i] = '\x0';
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:59:24 by jgermany          #+#    #+#             */
-/*   Updated: 2022/12/06 14:57:28 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/12/06 23:30:18 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
