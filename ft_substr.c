ad5d229e3231ab3badc3ee1ffe970d3d88d4318b
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2022-12-17 19:40:09 +0100

    Started debuging ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,22 +6,37 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:38:00 by jgermany          #+#    #+#             */
-/*   Updated: 2022/12/13 17:50:59 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/12/17 19:39:03 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// MEMORY LEAK SAFE OR NOT ?
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	size;
 
-	substr = malloc((len + 1) * sizeof(char));
+	// ft_substr_test.cpp
+
+	// if ((start < ft_strlen(s)) && (len > ft_strlen(s))) 
+	// 	size = (ft_strlen(s) - start);
+	// else if ((start < ft_strlen(s)) && (len <= ft_strlen(s)))
+	// 	size = (len - start);
+	// else
+	// 	size = 0; // +1 ??
+		
+	if (len > ft_strlen(s))
+		size = ft_strlen(s);
+	else
+		size = len;
+
+	substr = malloc((size + 1) * sizeof(char));
 	if (!substr)
 		return ((char *)0);
 	i = 0;
-	while ((i < len))
+	while ((i < len) && ((start + i) < ft_strlen(s)))
 	{
 		substr[i] = s[start + i];
 		i++;
