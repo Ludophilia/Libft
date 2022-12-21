63bfe30d35e7ff3283ae07f9674c6a6aae0c891c
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2022-12-21 13:55:10 +0100

    Corrected striteri and its infinite loop issue.

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,19 +6,21 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 16:13:31 by jgermany          #+#    #+#             */
-/*   Updated: 2022/12/10 16:23:15 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/12/21 13:49:25 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// MEMORY LEAK FREE OR WOT?
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
 	i = 0;
 	while (s[i])
+	{
 		f(i, s + i);
+		i++;
+	}
 	return ;
 }
