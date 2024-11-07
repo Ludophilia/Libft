d813c6af43ef5d3f2647fdbfdfb655d83b813ce3
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-07 12:36:28 +0100

    Added ft_strlen 2.

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jegerman <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/06 12:44:08 by jegerman          #+#    #+#              #
-#    Updated: 2024/11/07 11:52:36 by jegerman         ###   ########.fr        #
+#    Updated: 2024/11/07 12:28:17 by jegerman         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,6 +23,8 @@ SRCS += ft_isdigit.c
 SRCS += ft_isalnum.c
 SRCS += ft_isascii.c
 SRCS += ft_isprint.c
+
+SRCS += ft_strlen.c
 
 OBJS := $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:49:27 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/07 11:57:23 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:30:19 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+size_t	ft_strlen(char *s);
 
 #endif
