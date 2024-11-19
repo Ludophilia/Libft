83f2a9c0518922f2ba9ab26f9b06f0b67d5e2d2f
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-19 14:51:55 +0100

    Added ft_lstadd_front_bonus.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jegerman <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/06 12:44:08 by jegerman          #+#    #+#              #
-#    Updated: 2024/11/19 13:21:00 by jegerman         ###   ########.fr        #
+#    Updated: 2024/11/19 13:36:08 by jegerman         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -65,7 +65,7 @@ SRCS += ft_putnbr_fd.c
 OBJS := $(SRCS:.c=.o)
 
 SRCS_B := ft_lstnew_bonus.c
-# SRCS_B += ft_isalpha.c
+SRCS_B += ft_lstadd_front_bonus.c
 
 OBJS_B := $(SRCS_B:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:49:27 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/19 12:50:10 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/19 13:58:04 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
