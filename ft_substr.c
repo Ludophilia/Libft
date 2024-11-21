25aade055369627ff3a17b29713cf1beeb19ba97
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-21 16:30:42 +0100

    Stuck at substr, again

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:10:18 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/15 13:22:44 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/21 16:30:23 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,38 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	slen;
-	size_t	max_sublen;
+	size_t	sublen;
 	size_t	i;
 
-	if (s == NULL || len == 0)
+	if (s == NULL)
 		return (NULL);
 	slen = ft_strlen(s);
-	if (start >= slen)
-		return (NULL);
-	max_sublen = slen - start;
-	if (len < max_sublen)
-		max_sublen = len;
-	sub = ft_calloc(max_sublen + 1, sizeof(char));
+	// len == 0 case
+	// start >= slen case
+	// 
+
+	// if (start >= slen)
+	// 	return (NULL);
+
+
+	// 0 1 2 3
+	// start slen
+	sublen = 0;
+	while (start + sublen < slen && sublen <= len)
+		++sublen;
+
+	// sublen = slen - start;
+	// if (sublen < 0)
+	// 	sublen = 0;
+	// else if (len < sublen)
+	// 	sublen = len;
+
+
+	sub = ft_calloc(sublen + 1, sizeof(char));
 	if (sub == NULL)
 		return (NULL);
 	i = 0;
-	while (i < max_sublen)
+	while (i < sublen)
 	{
 		sub[i] = s[start + i];
 		++i;
