1ca22f67c763e7b9679db7abaf9fbd6a84b96edd
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-22 16:32:42 +0100

    Updated ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:28:56 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/12 17:07:32 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:29:34 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	while (i < n && (us1[i] || us2[i]))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (us1[i] != us2[i])
+			return (us1[i] - us2[i]);
 		++i;
 	}
 	return (0);
