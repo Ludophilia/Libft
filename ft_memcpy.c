d4a031442f7889746230b6dc188e2203c609b9c5
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-22 16:24:31 +0100

    Updated ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:33:09 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/11 14:59:16 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:12:16 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (dest == src)
+		return (dest);
 	i = -1;
 	while (++i < n)
 		((char *)dest)[i] = ((char *)src)[i];
