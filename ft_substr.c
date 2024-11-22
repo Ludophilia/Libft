ae49ac28968705be74b39a63f800185cdd347a19
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-22 15:29:03 +0100

    Updated ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:10:18 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/21 16:30:23 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:19:45 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	slen = ft_strlen(s);
-	// len == 0 case
-	// start >= slen case
-	// 
-
-	// if (start >= slen)
-	// 	return (NULL);
-
-
-	// 0 1 2 3
-	// start slen
 	sublen = 0;
-	while (start + sublen < slen && sublen <= len)
+	while (start + sublen < slen && sublen < len)
 		++sublen;
-
-	// sublen = slen - start;
-	// if (sublen < 0)
-	// 	sublen = 0;
-	// else if (len < sublen)
-	// 	sublen = len;
-
-
 	sub = ft_calloc(sublen + 1, sizeof(char));
 	if (sub == NULL)
 		return (NULL);
