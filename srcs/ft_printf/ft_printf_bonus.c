cfe3c79c77647833a0b0f09ecac0f3456e05dc05
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2025-02-11 17:21:55 +0100

    Added ft_dprintf, a prinft variation that writes on a targetted fd instead of the default 1

--- srcs/ft_printf/ft_printf_bonus.c
+++ srcs/ft_printf/ft_printf_bonus.c
@@ -6,11 +6,32 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:58:13 by jegerman          #+#    #+#             */
-/*   Updated: 2024/12/22 16:48:29 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:04:57 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
+
+int	ft_dprintf(int fd, const char *format, ...)
+{
+	t_meta	meta;
+	int		i;
+
+	if (format == NULL)
+		return (-1);
+	i = 0;
+	meta = (t_meta){.i = &i, .fd = fd, .count = 0};
+	va_start(meta.args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && is_valid_specif(format + ++i, &meta))
+			process_specifier(&meta);
+		else
+			ft_putchar_cc(format[i++], &meta);
+	}
+	va_end(meta.args);
+	return (meta.count);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -20,7 +41,7 @@ int	ft_printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	i = 0;
-	meta = (t_meta){.i = &i, .count = 0};
+	meta = (t_meta){.i = &i, .fd = 1, .count = 0};
 	va_start(meta.args, format);
 	while (format[i])
 	{
